8456502d8904d0caf9721044e059e5e945bcaf06
keeev90 <kvn.leman@gmail.com>
2021-10-13 16:01:54 +0200

    ajout dossier bot twitter

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+ruby '2.7.4'
+gem 'twitter'
+gem 'rspec'
+gem 'dotenv'
+gem 'pry'
---
+++ lib/bot_twitter.rb
@@ -0,0 +1,87 @@
+# ligne très importante qui appelle les gems.
+require 'twitter'
+require 'dotenv'
+require 'pry'
+
+# Dotenv : ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb) et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+Dotenv.load('.env')
+
+# arrays utilisées
+journalists = ["@jcunniet", "@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain"]
+
+# quelques lignes qui appellent les clés d'API de ton fichier .env
+def configure_rest_client 
+  rest_client = Twitter::REST::Client.new do |config| #You can use this client to make any Twitter REST API request.
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+end
+
+def configure_streaming_client 
+  streaming_client = Twitter::Streaming::Client.new do |config| #You can use this client to make any Twitter Streaming API request.
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+end
+
+#0. fonction qui permet de tweeter sur ton compte
+def test_tweet(rest_client)
+  rest_client.update('Mon premier tweet en Ruby !!!! @the_hacking_pro')
+end
+
+#1. say "hello" à 5 journalistes choisis au hasard parmi l'array
+def hello_random(rest_client)
+  five_random_journalists = journalists.sample(5)
+  five_random_journalists.each do |journalist_tweeter|
+   rest_client.update("Hello #{journalist_tweeter}! Nice job ! #bonjour_monde @the_hacking_pro")
+   puts "The bot sent a tweet to #{journalist_tweeter}"
+  end
+end
+
+#2. Liker les 25 derniers tweets qui ont un #bonjour_monde
+def like_tweet(rest_client)
+  rest_client.search("#bonjour_monde", result_type: "recent").take(25).collect do |tweet|
+    rest_client.favorite(tweet)
+    puts "The bot liked that tweet : #{tweet.text}"
+  end
+end
+
+#3. Follow les 20 dernières personnes qui ont tweeté avec #bonjour_monde
+def follow_people(rest_client)
+  rest_client.search('#bonjour_monde').take(20).uniq.each do |tweet|
+    if (tweet.user != client.user)
+      rest_client.follow(tweet.user)
+      puts "The bot followed @#{tweet.user.screen_name}"
+    end
+  end
+end
+
+#4. Utiliser la fonction Streaming de l'API Twitter afin de réagir en live (like et follow des comptes tweetant le #bonjour_monde)
+def live_follow_and_like(rest_client, streaming_client)
+  streaming_client.filter(track: "#bonjour_monde") do |tweet|
+    if tweet.is_a?(Twitter::Tweet) #Check if received tweet is an original tweet (autre exemple pour checker des messages reçus : Twitter::DirectMessage) 
+        rest_client.favorite(tweet)
+        rest_client.follow(tweet.user.id)
+        puts "The bot liked that tweet : #{tweet.text} and followed that tweeter : #{tweet.user.id}"
+    end
+  end
+end
+
+def perform #enlever les # devant les fonctions et le perform pour les faire fonctionner
+  rest_client = configure_rest_client
+  stream_stream = configure_stream_client
+  #test_tweet(rest_client)
+  #hello_random(rest_client)
+  #like_tweet(rest_client)
+  #follow_people(rest_client)
+  #live_follow_and_like(rest_client, streaming_client)
+end
+#perform
+
+
+
+  
